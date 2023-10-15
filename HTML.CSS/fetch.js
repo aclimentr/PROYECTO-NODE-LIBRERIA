5125ee37d4b209fd96a8289ea3976dd99dd260d5
alvaro <alvarocliment1999@gmail.com>
2023-10-15 11:47:32 +0200

    fetch and front upload

--- HTML.CSS/fetch.js
+++ HTML.CSS/fetch.js
@@ -1,6 +1,6 @@
 const arraybooks = [];
 let main$$ = document.querySelector("main")
-const input$$ = document.querySelector("input")
+const input$$ = document.getElementsByClassName("searchingbar")
 const getAllBooks = async () =>{
     for (let i = 1; i <= 151; i++){
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
@@ -11,15 +11,36 @@ const getAllBooks = async () =>{
     return arraybooks;
 }
 
-// const mapBooks = (books)=>{
-//     return books.map((results)=>({
-//         nombre: results.name,
-//         foto: results.sprites.front_default
-//     }))
-// }
+const mapBooks = (books)=>{
+    return books.map((results)=>({
+        nombre: ,
+        foto: ,
+        autor:
+    }))
+}
 
+    const printBooks = (books) =>{
+        main$$.innerHTML=""
+        for(const drawbooks of books){
+            const booksDiv$$ = document.createElement("div")
+            booksDiv$$.innerHTML=`
+            <h2>${drawbooks.name}</h2>
+            <img src="${drawbooks.image}" alt="${drawbooks.name}"
+            `;
+            main$$.appendChild(booksDiv$$);
+        }
+    }
+const useInput = (mapingBooks) => {
+    input$$.addEventListener("input", () =>searchBooks(input$$.value, mapingBooks))
+}
+const searchBooks = (filtro, books) =>{
+    let filteredBooks = books.filter((books)=>books.name.toLowercase().includes(filtro.toLowercase()))
+    printBooks(filteredBooks);
+}
 const init = async ()=>{
     const books = await getAllBooks();
-    const mappingBooks = mapBooks(books); 
+    const mapingBooks = mapBooks(books); 
+    console.log(mapingBooks);
+    printBooks(mapingBooks);
 }
 init();
